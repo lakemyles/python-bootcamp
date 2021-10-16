db38c270d5a5681b53e8d15775f9d29194e0897e
Myles Lake <lakemyles@gmail.com>
2021-10-15 19:05:42 -0500

    Add readme and day 1 notes.

---
+++ notes/day-1.py
@@ -0,0 +1,33 @@
+# print function
+# print("Hello world!")
+
+# Challenge 1.1
+# print("Day 1 - Python Print Function")
+# print("The function is delcared like this:")
+# print("print('what to print')")
+
+# new line character
+#print("Hello world!\nHello world!")
+
+# string concatenation
+#print("Hello " + "Myles")
+
+# indentation matters
+#    print("hey") # indentation causes an error #IndentationError
+
+# input function
+# input("What is your name? ")
+
+# Challenge 1.3
+# name = input("What is your name? ")N
+# nameLength = len(name)
+# print(nameLength)
+
+# Day 1 Project
+# Create a Python console program that greets the user,
+# asks them what city they grew up in, the name of their
+# first pet, and then shows them their band name.
+print("Hello! Welcome to the Band Name Generator!")
+userBirthCity = input("What city did you grow up in?\n")
+userPetName = input("What is the name of your first pet?\n")
+print("Your band name is " + userPetName + " " + userBirthCity + ".")
