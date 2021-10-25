cebdf94f5dfc695833581a398c9b1eaa99eec963
Myles Lake <lakemyles@gmail.com>
2021-10-25 16:25:49 -0500

    Add day 6 notes on functions

---
+++ notes/day_6.py
@@ -0,0 +1,25 @@
+# Functions and Code Blocks/Indentation
+
+# Define a function
+# Everything indented after colon is part of function
+# Function definitions are not hoisted and
+# have to be called after function definition
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+#     print("hi")
+
+
+# # iterate 6 times
+# for step in range(6):
+#     print(step)
+
+# iterate 6 times
+# i = 0
+# while 6 > i:
+#     print(i)
+#     i += 1
+
+# Infinite loop
+# while True:
+#     print("INFINITY AND BEYOND")
