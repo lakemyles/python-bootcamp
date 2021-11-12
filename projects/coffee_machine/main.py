79a921ee80df5e2605be766751505866afe53ebd
Myles Lake <lakemyles@gmail.com>
2021-11-11 21:46:52 -0600

    Refactor variable names for semantic meaning

--- projects/coffee_machine/main.py
+++ projects/coffee_machine/main.py
@@ -2,20 +2,20 @@ import coffee_machine_ui
 from coffee_machine import validate_payment, process_order, validate_resources
 
 
-do_continue = True
+is_on = True
 
-while do_continue:
+while is_on:
     user_selection = coffee_machine_ui.get_user_selection()
     if user_selection == "off":
-        do_continue = False
+        is_on = False
     elif user_selection == "report":
         coffee_machine_ui.display_report()
     else:
-        is_sufficient_resources = validate_resources(user_selection)
-        if is_sufficient_resources:
+        has_sufficient_resources = validate_resources(user_selection)
+        if has_sufficient_resources:
             user_payment = coffee_machine_ui.get_user_payment()
-            is_sufficient_payment = validate_payment(user_selection, user_payment)
-            if is_sufficient_payment:
+            is_payment_successful = validate_payment(user_selection, user_payment)
+            if is_payment_successful:
                 process_order(user_selection, user_payment)
             else:
                 coffee_machine_ui.display_insufficient_payment()
